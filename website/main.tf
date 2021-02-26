8903526ba2a926849d6f338ac06ed14fcc596cc0
Matthew Cascio <cascio@cybercapsicum.com>
2021-02-26 17:49:44 +0000

    Use modern terraform syntax

--- website/main.tf
+++ website/main.tf
@@ -1,11 +1,10 @@
 provider "cloudflare" {
-  version = "~> 2.0"
-  api_key = "${var.cloudflare_api_token}"
+  api_key = var.cloudflare_api_token
 }
 
 resource "cloudflare_worker_route" "base_domain_route" {
-  zone_id = "${var.cloudflare_zone_id}"
-  pattern = "${var.cloudflare_website}"
+  zone_id = var.cloudflare_zone_id
+  pattern = var.cloudflare_website
   script_name = cloudflare_worker_script.my_script.name
 }
 
