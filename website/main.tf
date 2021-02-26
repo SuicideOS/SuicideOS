755d9841a3fe6755385594590188551d9f2060f6
Matthew Cascio <cascio@cybercapsicum.com>
2021-02-26 17:48:10 +0000

    Setup main.tf with basic defaults

--- website/main.tf
+++ website/main.tf
@@ -2,3 +2,14 @@ provider "cloudflare" {
   version = "~> 2.0"
   api_key = "${var.cloudflare_api_token}"
 }
+
+resource "cloudflare_worker_route" "base_domain_route" {
+  zone_id = "${var.cloudflare_zone_id}"
+  pattern = "${var.cloudflare_website}"
+  script_name = cloudflare_worker_script.my_script.name
+}
+
+resource "cloudflare_worker_script" "cli_agent" {
+  name = "cli_agent"
+  content = file("src/serverless/cli_agent.js")
+}
