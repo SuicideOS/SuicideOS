3a6603a831d1d575349cb61043f19312f93cc1b6
Matthew Cascio <cascio@cybercapsicum.com>
2021-02-26 18:02:39 +0000

    Add cloudflare_account_id

--- website/main.tf
+++ website/main.tf
@@ -8,12 +8,15 @@ terraform {
 
 variable "cloudflare_api_token" { }
 
+variable "cloudflare_account_id" { }
+
 variable "cloudflare_zone_id" { }
 
 variable "cloudflare_website" { }
 
 provider "cloudflare" {
   api_key = var.cloudflare_api_token
+  account_id = var.cloudflare_account_id
 }
 
 resource "cloudflare_worker_route" "base_domain_route" {
