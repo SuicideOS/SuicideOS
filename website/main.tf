e9dda1173b926b717afe55d3e0637bd01aa15594
Matthew Cascio <cascio@cybercapsicum.com>
2021-02-26 14:55:15 -0500

    Run terraform fmt

--- website/main.tf
+++ website/main.tf
@@ -7,17 +7,17 @@ terraform {
 }
 
 provider "cloudflare" {
-  api_token = var.cloudflare_api_token
+  api_token  = var.cloudflare_api_token
   account_id = var.cloudflare_account_id
 }
 
 resource "cloudflare_worker_route" "base_domain_route" {
-  zone_id = var.cloudflare_zone_id
-  pattern = var.cloudflare_website
+  zone_id     = var.cloudflare_zone_id
+  pattern     = var.cloudflare_website
   script_name = cloudflare_worker_script.cli_agent.name
 }
 
 resource "cloudflare_worker_script" "cli_agent" {
-  name = "cli_agent"
+  name    = "cli_agent"
   content = file("src/serverless/cli_agent.js")
 }
