6947decbeaee755e2b966047aa8c909da3495b51
Matthew Cascio <cascio@cybercapsicum.com>
2021-02-27 03:12:55 +0000

    Update worker index (#11)
    ## Changelog
    
    1. Add basic worker index.js

--- website/index.js
+++ website/index.js
@@ -1,12 +1,48 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello world text
- * @param {Request} request
- */
-async function handleRequest(request) {
-  return new Response('Hello world!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+const githubApiUrl = "https://api.github.com/repos/"
+const githubUrl = "https://github.com/"
+const repo = "SuicideOS/SuicideOS"
+const defaultTag = "0.1.0"
+
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get("content-type") || ""
+  if (contentType.includes("application/json")) {
+    return JSON.stringify(await response.json())
+  }
+  else if (contentType.includes("application/text")) {
+    return await response.text()
+  }
+  else if (contentType.includes("text/html")) {
+    return await response.text()
+  }
+  else {
+    return await response.text()
+  }
 }
+
+async function getLatestReleaseTag() {
+  const url = githubApiUrl + repo + "/releases/latest"
+  const init = {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      "User-Agent": "Request",
+    },
+  }
+  const response = await fetch(url, init)
+  const release = await gatherResponse(response)
+
+  return await JSON.parse(release).tag_name || defaultTag
+}
+
+async function handleRequest() {
+  const latestTag = await getLatestReleaseTag()
+  const fileUrl = githubUrl + repo + "/releases/download/" + latestTag + "/install.sh.asc"
+  const response = await fetch(fileUrl)
+  const signedScript = await gatherResponse(response)
+
+  return new Response(signedScript)
+}
+
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest())
+})
